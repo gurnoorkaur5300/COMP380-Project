65c0cb994766d8360ec60dd540f9fef53b8612ce
greg <greg87peterson@gmail.com>
2024-03-19 22:20:16 -0700

    updated controller class and updated navbar

--- controller.py
+++ controller.py
@@ -18,38 +18,62 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         
-        #navbar assigned to top of each page
-        self.navbar = NavigationBar(self,self)
-        self.navbar.pack(side="top", fill="x")
+        #minimum window size when app opens
+        self.minsize(width=800, height=600)
         
-        container = tk.Frame(self)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0,weight=1)
-        container.grid_columnconfigure(0,weight=1)
+        # Configure main window to expand and contract proportionally
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)  # Container row
+        self.grid_columnconfigure(0, weight=1)
         
+          
+        # Header frame
+        self.headerFrame = tk.Frame(self, bg="beige")
+        self.headerFrame.grid(row=0, column=0, sticky="ew")  
+        self.headerFrame.grid_columnconfigure(0, weight=1)  # Ensure the column expands to fill the space
+        self.headerFrame.grid_columnconfigure(1, weight=0)  # Ensure the column with the label doesn't expand
+        
+        # # Header label text
+        self.homeLabel = tk.Label(self.headerFrame, text="Titan Hotel Reservations", bg="beige", fg="gray17", height=2, padx=20, pady=5, font=("Ariel",28))
+        self.homeLabel.grid(row=0, column=1, sticky="e")
+          
+        # Create a container frame for pages
+        self.container = tk.Frame(self)
+        self.container.grid(row=1, column=0, sticky="nsew")
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+        
+        # Instantiate pages
+        self.homePage = Home(self.container, self)
+        self.policiesPage = Policies(self.container, self)
+        self.accountPage = Account(self.container, self)
+     
         self.frames = {}
         
-        
-        #add pages to tuple
-        #Login, Create,  Room, View, Cancel, 
-        for F in ( Home, Policies, Account):
-            pageName = F.__name__
-            #for each page we need an instance of that class
-            frame = F(container,self)
-            #add each page to self.frames dictionary 
-            self.frames[pageName] = frame
-            frame.grid(row=0,column=0,sticky="nsew")
-            
-        #by default, display home page
+        # By default, display home page
         self.showFrame("Home")
      
-    #show frame function   
+    # Show frame function   
     def showFrame(self, pageName):
-        frame=self.frames[pageName]
-        frame.show()
+        def showNavbar():
+            self.navbar = NavigationBar(self, self, initialState=False)
+            self.navbar.grid(row=0, column=0, sticky="w", padx=20) 
+        if pageName == "Home" or pageName == "Policies" or pageName == "Account":
+            showNavbar() 
+           
+        if pageName == "Home":
+            self.homePage.grid(row=0, column=0, sticky="nsew")
+            self.homePage.tkraise()
+        elif pageName == "Policies":
+            self.policiesPage.grid(row=0, column=0, sticky="nsew")  
+            self.policiesPage.tkraise()
+        elif pageName == "Account":
+            self.accountPage.grid(row=0, column=0, sticky="nsew")  
+            self.accountPage.tkraise()
 
-#creates instance of App class and starts GUI      
+
+# Creates instance of App class and starts GUI      
 if __name__ == "__main__":
     app = App()
+    app.title("Titan Reservations")
     app.mainloop()
-            
--- navigationBar.py
+++ navigationBar.py
@@ -1,21 +1,59 @@
+from tkinter import PhotoImage
 import tkinter as tk
 
 class NavigationBar(tk.Frame):
-    def __init__(self,parent,controller):
-        tk.Frame.__init__(self,parent)
+    def __init__(self, parent, controller, initialState=False):
+        super().__init__(parent)
         self.controller = controller
         
-        #define homeButton variable and style home button
-        homeButton = tk.Button(self, text="Home", command=lambda: controller.showFrame("Home"))
-        homeButton.grid(row=0,column=0,padx=10,pady=5)
+        # Dictionary of colors
+        self.color = {"nero": "#252726", "beige": "#F5F5DC", "orange": "#FF8700", "darkorange": "#FE6101"}
+
+        # Setting switch start
+        self.btnState = initialState  
+
+        self.navbarBtn = tk.Button(self, text="☰", bg=self.color["nero"], activebackground=self.color["nero"], bd=0, height=3,padx=20, command=self.switch)
+        self.navbarBtn.grid(row=0, column=0, sticky="nw")
+
+        # Setting Navbar frame
+        self.navRoot = tk.Frame(parent, bg=self.color["nero"], height=500, width=300)
+
+        # Set y coord of navbar widgets
+        y = 80
+        # Options in navbar
+        options = ["Home", "Account", "Policies"]
         
-        #define accountButton variable and style account button
-        accountButton = tk.Button(self, text="Account", command=lambda: controller.showFrame("Account"))
-        accountButton.grid(row=0, column=1, padx=10, pady=5)
+        #for each option in the navbar, when an option is selected, switch tothat page
+        for option in options:
+            def switchPage(page=option):
+                self.switch()
+                self.controller.showFrame(page)
+                
+            tk.Button(self.navRoot, text=option, bg=self.color["nero"], fg=self.color["orange"], activebackground=self.color["nero"], activeforeground="green", bd=0,command=switchPage).place(x=25, y=y)
+            y += 40
         
-        #define policiesButton variable and style account button
-        policiesButton = tk.Button(self, text="Policies", command=lambda: controller.showFrame("Policies"))
-        policiesButton.grid(row=0, column=2, padx=10, pady=5)
+        # Navbar close button
+        self.closeBtn = tk.Button(self.navRoot, bg=self.color["orange"], activebackground=self.color["orange"], text="✖", bd=0, command=self.switch)
+        self.closeBtn.place(x=250, y=10)
         
-        
-    
+        if initialState:
+            self.navRoot.grid(row=1, column=0, sticky="nsw")
+
+    def switch(self):
+        if self.btnState is True:
+            # Close navbar animation
+            for x in range(0,301,5):
+                self.navRoot.place(x=-x, y=0)
+                self.update()
+
+            # Turn button off
+            self.btnState = False
+        else:
+            # Open navbar animation
+            for x in range(-301, 0, 5):
+                self.navRoot.place(x=x, y=0)
+                self.update()
+                self.navbarBtn.config(bg=self.color["nero"],activebackground=self.color["nero"])
+            self.btnState = True
+          
+   
