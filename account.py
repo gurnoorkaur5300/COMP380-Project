9d46f8b45ce6e504685968b3abb282d651b2dc91
greg <greg87peterson@gmail.com>
2024-04-08 15:18:50 -0700

    update customer class to include getter, setters, private

--- account.py
+++ account.py
@@ -9,13 +9,18 @@ class Account(Page):
         
          #test customer case
         customerInfo = {
-            "name": "Greg",
-            "email": "gregory.calderon.514@my.csun.edu",
-            "dob": "02/06/1987",
-            "reservations": ["2jaifj", "33roij", "8a9a98"]
+            "n_name": "Greg",
+            "n_email": "gregory.calderon.514@my.csun.edu",
+            "n_dob": "02/06/1987",
+            "n_reservations": ["2jaifj", "33roij", "8a9a98"]
         } 
         
-        self.customer = Customer(**customerInfo)
+        self.customer = Customer(
+            customerInfo["n_name"],
+            customerInfo["n_email"],
+            customerInfo["n_dob"],
+            customerInfo["n_reservations"]
+        )
 
 
         
@@ -40,16 +45,16 @@ class Account(Page):
         userReservationsLabel.grid(row=4, column=0, padx=15,sticky="e")
 
         #column 1 is data of customer
-        userNameLabel = tk.Label(infoBox,text=self.customer.name, bg="white", fg="black", anchor="w", width=50)
+        userNameLabel = tk.Label(infoBox,text=self.customer.getName(), bg="white", fg="black", anchor="w", width=50)
         userNameLabel.grid(row=0, column=1, pady=15, sticky="w")
 
-        userEmailLabel = tk.Label(infoBox,text=self.customer.email, bg="white", fg="black", anchor="w", width=50)
+        userEmailLabel = tk.Label(infoBox,text=self.customer.getEmail(), bg="white", fg="black", anchor="w", width=50)
         userEmailLabel.grid(row=1, column=1, pady=15, sticky="w")
 
-        userDOBLabel = tk.Label(infoBox,text=self.customer.dob, bg="white", fg="black", anchor="w", width=15)
+        userDOBLabel = tk.Label(infoBox,text=self.customer.getDob(), bg="white", fg="black", anchor="w", width=15)
         userDOBLabel.grid(row=2, column=1, pady=15, sticky="w")
         
-        for i, reservation in enumerate(self.customer.reservations):
+        for i, reservation in enumerate(self.customer.getReservations()):
             userReservationsLabel = tk.Label(infoBox, text=reservation, anchor="w", bg="white", fg="black", width=50)
             userReservationsLabel.grid(row=i+4, column=1, pady=10, sticky="w")
 
