de892733708712447fc69163a86bba9ff8470a11
greg <greg87peterson@gmail.com>
2024-04-10 21:57:25 -0700

    navBar file using macOSX widgets

--- navigationBar.py
+++ navigationBar.py
@@ -1,5 +1,8 @@
 from tkinter import PhotoImage
 import tkinter as tk
+# from tkmacosx import Button as MacButton
+import tkmacosx
+
 
 class NavigationBar(tk.Frame):
     def __init__(self, parent, controller, initialState=False):
@@ -17,18 +20,18 @@ class NavigationBar(tk.Frame):
             self.headerFrame.grid_columnconfigure(col, weight=1) 
         
         # Header label text
-        self.homeLabel = tk.Label(self.headerFrame, text="Titan Hotel Reservations", bg="beige", fg="gray17", height=2, padx= 20, pady=5, font=("Ariel",28))
+        self.homeLabel = tk.Label(self.headerFrame, text="Titan Hotel Reservations", bg="beige", fg=self.color["nero"], height=2, padx= 20, pady=5, font=("Ariel",28))
         self.homeLabel.grid(row=0, column=1, sticky="e")
         
         # Setting switch start
         self.btnState = initialState  
 
         #main navigation bar button. when clicked will call switch() which animates the bar open and closed
-        self.navbarBtn = tk.Button(self.headerFrame, text="☰", bg=self.color["nero"], activebackground=self.color["nero"], bd=0, height=3,padx=20, pady=10,command=self.switch)
-        self.navbarBtn.grid(row=0, column=0, padx=10, pady=(10,10),sticky="nw")
+        self.navbarBtn = tkmacosx.Button(self.headerFrame, text="☰", activebackground="blue", bd=0, height=70, width= 75, padx=20, pady=10, command=self.switch, font=("Helvetica", 40))
+        self.navbarBtn.grid(row=0, column=0, padx=10, pady=(10,10), sticky="nw")
 
         # Setting Navbar frame
-        self.navRoot = tk.Frame(parent, bg=self.color["nero"], height=400, width=300)
+        self.navRoot = tk.Frame(parent, bg="beige", height=350, width=300)
 
         # Set y coord of navbar widgets
         y = 80
@@ -43,12 +46,12 @@ class NavigationBar(tk.Frame):
                 self.controller.showFrame(page)
                 
             #each item in navbar menu is a button. when the button in the navbar is clicked, that page should be populated using the function switchPage()
-            tk.Button(self.navRoot, text=option, bg=self.color["nero"], fg=self.color["orange"], activebackground=self.color["orange"], activeforeground="green", bd=0,command= switchPage, width=20, height=2, padx=20, pady=10).place(x=25, y=y)
+            tkmacosx.Button(self.navRoot, text=option, fg=self.color["nero"], activeforeground="blue", bd=0,command= switchPage, width=200, height=40,font=("Helvetica", 20)).place(x=50, y=y)
             y += 80
                 
         # Navbar close button
-        self.closeBtn = tk.Button(self.navRoot, bg=self.color["orange"], activebackground=self.color["orange"], text="✖", bd=0, command=self.switch)
-        self.closeBtn.place(x=250, y=10)
+        self.closeBtn = tkmacosx.Button(self.navRoot, text="✖", bd=0, command=self.switch, fg=self.color["nero"],font=("Helvetica", 14), padx=-20)
+        self.closeBtn.place(x=230, y=10)
         
         #if the 
         if initialState:
@@ -68,7 +71,7 @@ class NavigationBar(tk.Frame):
             for x in range(-301, 0, 5):
                 self.navRoot.place(x=x, y=0)
                 self.update_idletasks()
-                self.navbarBtn.config(bg=self.color["nero"],activebackground=self.color["nero"])
+                # self.navbarBtn.config(bg=self.color["orange"],activebackground=self.color["nero"])
             self.btnState = True
           
    
