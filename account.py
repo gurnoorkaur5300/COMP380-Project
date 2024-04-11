97f757e09c10f8f5c9b855150530b874da7e0c25
greg <greg87peterson@gmail.com>
2024-04-11 01:10:18 -0700

    fixed bug in account

--- account.py
+++ account.py
@@ -45,16 +45,16 @@ class Account(Page):
         userReservationsLabel.grid(row=4, column=0, padx=15,sticky="e")
 
         #column 1 is data of customer
-        userNameLabel = tk.Label(infoBox,text=self.customer.getName(), bg="white", fg="black", anchor="w", width=50)
+        userNameLabel = tk.Label(infoBox,text=self.customer.getName, bg="white", fg="black", anchor="w", width=50)
         userNameLabel.grid(row=0, column=1, pady=15, sticky="w")
 
-        userEmailLabel = tk.Label(infoBox,text=self.customer.getEmail(), bg="white", fg="black", anchor="w", width=50)
+        userEmailLabel = tk.Label(infoBox,text=self.customer.getEmail, bg="white", fg="black", anchor="w", width=50)
         userEmailLabel.grid(row=1, column=1, pady=15, sticky="w")
 
-        userDOBLabel = tk.Label(infoBox,text=self.customer.getDob(), bg="white", fg="black", anchor="w", width=15)
+        userDOBLabel = tk.Label(infoBox,text=self.customer.getDob, bg="white", fg="black", anchor="w", width=15)
         userDOBLabel.grid(row=2, column=1, pady=15, sticky="w")
         
-        for i, reservation in enumerate(self.customer.getReservations()):
+        for i, reservation in enumerate(self.customer.getReservations):
             userReservationsLabel = tk.Label(infoBox, text=reservation, anchor="w", bg="white", fg="black", width=50)
             userReservationsLabel.grid(row=i+4, column=1, pady=10, sticky="w")
 
