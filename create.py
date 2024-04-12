22b82c02f39eaee2e099961ffdbc103dc337570d
greg <greg87peterson@gmail.com>
2024-04-12 12:23:03 -0700

    added logic and verification

--- create.py
+++ create.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 from tkinter import messagebox
-# from pageHeader import PageHeader
+import hashlib
+import re
+from customer import Customer
+from database import Database
 
 class Create(tk.Toplevel):
-    def __init__(self,controller, master=None):
+    def __init__(self,controller, database, master=None):
         super().__init__(master)
         self.controller= controller
+        self.database = database 
         self.title("Create Account")
         self.geometry("600x600")
 
@@ -14,6 +18,7 @@ class Create(tk.Toplevel):
         self.userLastName = tk.Entry(self, width=30,font = ("Arial",20), bg="white", fg="black")
         self.userDOB = tk.Entry(self, width =30, font=("Arial", 20), bg="white", fg="black")
         self.userEmail = tk.Entry(self, width = 30, font = ("Arial", 20), bg="white", fg="black")
+        self.userPhone = tk.Entry(self, width = 30, font = ("Arial", 20), bg="white", fg="black")
         self.userPassword = tk.Entry(self,width = 30, font = ("Arial",20), bg="white", fg="black")
         self.userPasswordConfirm = tk.Entry(self, width =30, font =("Arial", 20), bg="white", fg="black")
         self.securityWord = tk.Entry(self, font = ("Arial", 20), bg ="white", fg ="black")
@@ -29,6 +34,9 @@ class Create(tk.Toplevel):
         
         self.userEmail.insert(0,"Enter email")
         self.userEmail.defaultText = "Enter email"
+
+        self.userPhone.insert(0,"Enter Phone")
+        self.userPhone.defaultText = "Enter Phone"
         
         self.userPassword.insert(0,"Enter password")
         self.userPassword.defaultText = "Enter password"
@@ -45,12 +53,13 @@ class Create(tk.Toplevel):
         self.userLastName.pack(pady=(20,20))
         self.userDOB.pack(pady=(20,20))
         self.userEmail.pack(pady=(20,20))
+        self.userPhone.pack(pady=(20,20))
         self.userPassword.pack(pady=(20,20))
         self.userPasswordConfirm.pack(pady =(20,20))
    
 
         #create button to submit
-        submitButton = tk.Button(self, text = "SUBMIT", borderwidth=10, font=("Arial", 22), bg = "white", fg ="black", command= self.passwordMatch)
+        submitButton = tk.Button(self, text = "SUBMIT", borderwidth=10, font=("Arial", 22), bg = "white", fg ="black", command= self.getData)
         submitButton.pack(padx = 200, side = "left")
 
         #bid clearEntries method to the FOCUSIN for all widgest that need to be cleared
@@ -58,12 +67,11 @@ class Create(tk.Toplevel):
         self.userLastName.bind("<FocusIn>", self.clearEntries)
         self.userDOB.bind("<FocusIn>",self.clearEntries)
         self.userEmail.bind("<FocusIn>", self.clearEntries)
-        # self.userPassword.bind("<FocusIn>", self.clearEntries)
-        # self.userPasswordConfirm.bind("<FocusIn>", self.clearEntries)
-        self.userPassword.bind("<FocusIn>", self.handlePasswordFocusIn)
-        self.userPassword.bind("<FocusOut>", self.handlePasswordFocusOut)
-        self.userPasswordConfirm.bind("<FocusIn>", self.handleConfirmPasswordFocusIn)
-        self.userPasswordConfirm.bind("<FocusOut>", self.handleConfirmPasswordFocusOut)
+        self.userPhone.bind("<FocusIn>", self.clearEntries)
+        # self.userPassword.bind("<FocusIn>", self.handlePasswordFocusIn)
+        # self.userPassword.bind("<FocusOut>", self.handlePasswordFocusOut)
+        # self.userPasswordConfirm.bind("<FocusIn>", self.handleConfirmPasswordFocusIn)
+        # self.userPasswordConfirm.bind("<FocusOut>", self.handleConfirmPasswordFocusOut)
 
         self.update_idletasks()
 
@@ -75,15 +83,67 @@ class Create(tk.Toplevel):
         if currentText == defaultText:
             entryBox.delete(0, tk.END)
 
-     #Create function that veryfies if Password and Confirm Password match
+      #Create function that veryfies if Password and Confirm Password match
     def passwordMatch(self):
         passWord = self.userPassword.get()
         passWordConfirma = self.userPasswordConfirm.get()
-        if passWord == passWordConfirma:
-            self.showSuccessMessage("Successful","Account Created") 
-            self.destroy()
-        else:
-            self.showSuccessMessage("Error", "Passwords do not match, Press OK and try again")   
+        if passWord != passWordConfirma:
+            self.showSuccessMessage("Error", "Passwords do not match, Press OK and try again") 
+            return False
+        return True
+            
+
+    def getData(self):
+        firstName = self.userName.get()
+        lastName = self.userLastName.get()
+        dob = self.userDOB.get()
+        phone = self.userPhone.get()
+        email = self.userEmail.get()
+        passwrd = self.userPassword.get()
+
+        if not self.validateName(firstName, lastName):
+            return False
+        if not self.validateEmail(email):
+            return False
+        if not self.validatePassword(passwrd):
+            return False
+
+        name = firstName + " " + lastName
+        hashPasswrd = hashlib.sha256(passwrd.encode()).hexdigest()
+
+        newCustomer = Customer(name, email, dob, phone, hashPasswrd)
+
+        self.database.insertCustomer(newCustomer)
+
+        return True
+
+    def validateName(self, firstN, lastN):
+        if not firstN.strip() or not lastN.strip():
+            messagebox.showerror("Error", "Please enter both a first and last name")
+            return False
+        return True
+
+    def validateEmail(self, email):
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(pattern, email):
+            messagebox.showerror("Error", "Please enter a valid email address")
+            return False
+        return True
+
+    def validatePassword(self, passwrd):
+        if len(passwrd) < 8:
+            messagebox.showerror("Error", "Password must be at least 8 characters long")
+            return False
+
+        if not re.search(r'[A-Z]', passwrd) or not re.search(r'[a-z]', passwrd) or not re.search(r'\d',passwrd):
+            messagebox.showerror("Error", "Password must contain 1 upper case letter, 1 lower case letter, and 1 digit")
+            return False
+        
+        if not self.passwordMatch():
+            return False
+        
+        return True
+
     
     #function that displays success message
     def showSuccessMessage(self, title, message):
