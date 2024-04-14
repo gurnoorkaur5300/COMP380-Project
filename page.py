61df10a42eaa753b50d95a49335cc88553ec266f
greg <greg87peterson@gmail.com>
2024-04-14 02:30:58 -0700

    docs for page.py

--- page.py
+++ page.py
@@ -2,12 +2,24 @@ import tkinter as tk
 
 #base class for individual pages
 class Page(tk.Frame):
+    """
+    Base class for individual pages in a tkinter application.
+    :author: Gregory Calderon
+    :version: 1
+
+    Attributes:
+        controller: The controller responsible for managing page transitions.
+    """
     #function to initialize new instance of class
     def __init__(self, parent, controller):
+        """
+        Initializes a new instance of the Page class.
+
+        Args:
+            parent: The parent widget to which this page belongs.
+            controller: The controller responsible for managing page transitions.
+        """
         tk.Frame.__init__(self,parent)
         self.controller = controller
         
-    # switch page/ frame
-    # def show(self):
-    #     self.lift()
         
